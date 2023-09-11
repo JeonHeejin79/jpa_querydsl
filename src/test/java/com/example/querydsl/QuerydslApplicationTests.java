a87be65a36be5c4b3436757986c38c025e2292cc
SKMNS\202110043 <jeonheejin@sk.com>
2023-09-11 09:18:23 +0900

    first commit

---
+++ src/test/java/com/example/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,43 @@
+package com.example.querydsl;
+
+import com.example.querydsl.entity.Hello;
+import com.example.querydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class QuerydslApplicationTests {
+
+    // @PersistenceContext
+    @Autowired
+    EntityManager em;
+
+    @Test
+    @Rollback(value = false)
+    void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = new QHello("h");
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
+    }
+
+}
